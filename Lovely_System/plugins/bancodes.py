028400424c6a8968b92ce7933c22f08eec6ac4c9
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-08 15:14:00 +0530

    Update bancodes.py

--- Lovely_System/plugins/bancodes.py
+++ Lovely_System/plugins/bancodes.py
@@ -1,24 +1,24 @@
 help_plus = """ Here is Help for Bancodes  :-
-✯ • {LYx00} - Scammer 
-✯ • {LYx01} - SPAM ADDING MEMBER
-✯ • {LYx02} - CHILD ABUSE
-✯ • {LYx03} - ILLEGAL
-✯ • {LYx04} - FRAUD PROMOTION  [ANY KIND]
-✯ • {LYx05} - PHISHING
-✯ • {LYx06} - BAN EVASION
-✯ • {LYx07} - RAID/SPAM INFLAMED
-✯ • {LYx08} - ADDING SPAMBOTS
-✯ • {LYx09} - KRIMINALANT
-✯ • {LYx10} - SCAMMER
-✯ • {LYx11} - ABUSE SPAM
-✯ • {LYx12} - IMPERSONATION
-✯ • {LYx13} - MD/BTC SCAM
-✯ • {LYx14} - RAID INITIALIZER
-✯ • {LYx15} - RAID PARTICIPANT
-✯ • {LYx16} - SPAMBOT
-✯ • {LYx17} - CYBER THREATENING / CYBER BULLY
-✯ • {LYx18} - NSFW SPAMMER
+✯ • {SNYx00} - Scammer 
+✯ • {SNYx01} - SPAM ADDING MEMBER
+✯ • {SNYx02} - CHILD ABUSE
+✯ • {SNYx03} - ILLEGAL
+✯ • {SNYx04} - FRAUD PROMOTION  [ANY KIND]
+✯ • {SNYx05} - PHISHING
+✯ • {SNYx06} - BAN EVASION
+✯ • {SNYx07} - RAID/SPAM INFLAMED
+✯ • {SNYx08} - ADDING SPAMBOTS
+✯ • {SNYx09} - KRIMINALANT
+✯ • {SNYx10} - SCAMMER
+✯ • {SNYx11} - ABUSE SPAM
+✯ • {SNYx12} - IMPERSONATION
+✯ • {SMYx13} - MD/BTC SCAM
+✯ • {SNYx14} - RAID INITIALIZER
+✯ • {SNYx15} - RAID PARTICIPANT
+✯ • {SNYx16} - SPAMBOT
+✯ • {SNYx17} - CYBER THREATENING / CYBER BULLY
+✯ • {SNYx18} - NSFW SPAMMER
 
-Powered By  Lovely Support
+Powered By  SENKU Support
 """
 __plugin_name__ = "BanCodes" 
