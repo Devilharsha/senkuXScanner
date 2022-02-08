3b66fdcdb81c33d92d0958a863aef468a6fc4bbc
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-08 15:24:13 +0530

    Update gbans.py

--- Lovely_System/plugins/Mongo_DB/gbans.py
+++ Lovely_System/plugins/Mongo_DB/gbans.py
@@ -1,8 +1,8 @@
-from Lovely_System import MONGO_CLIENT
+from Senku_System import MONGO_CLIENT
 from typing import Optional, Dict, Union
 from datetime import datetime
 
-db = MONGO_CLIENT["Lovely"]["Main"]
+db = MONGO_CLIENT["Senku"]["Main"]
 
 
 async def get_gban(user: int) -> Optional[Dict[str, Union[str, int]]]:
