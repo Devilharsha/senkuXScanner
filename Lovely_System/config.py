cb0a26e2209f37fc721aede60e88bd43e4b91916
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-08 15:33:03 +0530

    Update config.py

--- Lovely_System/config.py
+++ Lovely_System/config.py
@@ -3,8 +3,8 @@ from typing import Union, Optional
 API_ID: int = 12345
 API_HASH: str = "XYZ"
 STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Lovely_logs: Union[str, int] = -1001512548429
-Lovely_approved_logs: Union[str, int] = -1001476531091
+Senku_logs: Union[str, int] = -1001512548429
+Senku_approved_logs: Union[str, int] = -1001476531091
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
 ENFORCERS: int = 123488
