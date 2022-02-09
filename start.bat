8e1138b207282be021cf742ffbcf7a8bd71d332d
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-09 16:12:31 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Senku_System
-:: Enables virtual env mode and then starts Senku_System
-env\scripts\activate.bat && py -m Senku_System
+TITLE Lovely_System
+:: Enables virtual env mode and then starts Lovely_System
+env\scripts\activate.bat && py -m Lovely_System
