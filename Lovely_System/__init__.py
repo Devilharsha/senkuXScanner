40b66d0b476e67fc57aeef95d0586d8d6776eb20
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-09 15:59:35 +0530

    Update __init__.py

--- Lovely_System/__init__.py
+++ Lovely_System/__init__.py
@@ -43,7 +43,7 @@ else:
     API_HASH_KEY = Config.API_HASH
     STRING_SESSION = Config.STRING_SESSION
     MONGO_DB_URL = Config.MONGO_DB_URL
-    with open(os.path.join(os.getcwd(), "Senku_System/elevated_users.json"), "r") as f:
+    with open(os.path.join(os.getcwd(), "Lovely_System/elevated_users.json"), "r") as f:
         data = json.load(f)
     Senku = data["Senku"]
     ENFORCERS = data["ENFORCERS"]
